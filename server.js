a91378940f009d2f86cfbd0d15fa4c276426f17d
JonnyRiver <jonnyriver6@gmail.com>
2022-08-15 19:38:55 -0400

    trying to push to main

--- server.js
+++ server.js
@@ -16,14 +16,13 @@ app.use(express.json());
 
 app.use(express.static("public"));
 
-mongoose.connect(MONGODB_URI, {
-  useNewUrlParser: true,
-  useFindAndModify: false,
-});
-
-// routes
-//app.use(require("./routes/api.js"));
-
 app.listen(PORT, () => {
-  console.log(`App running on port ${PORT}!!!!!!!`);
+  console.log("APP RUNNIN ON PORT 3001!!!!!!!!");
 });
+
+//mongoose.connect(MONGODB_URI, {
+//  useNewUrlParser: true,
+//  useFindAndModify: false,
+//});
+
+//app.use(require("./routes/api.js"));
