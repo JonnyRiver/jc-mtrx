6b92b220d6690aa1c46b67bbad7d045966597470
Corey Martin <53289343+coreymmartin@users.noreply.github.com>
2022-09-08 14:26:44 -0400

    Update server.js

--- server.js
+++ server.js
@@ -21,7 +21,7 @@ app.listen(PORT, () => {
 
 // new mongo stuff
 const { MongoClient, ServerApiVersion } = require('mongodb');
-const uri = "mongodb+srv://m001-student:m001-mongodb-basics@sandbox.rqg9kbv.mongodb.net/?retryWrites=true&w=majority";
+const uri = "mongodb+srv://m001-student:PASSWORD@sandbox.rqg9kbv.mongodb.net/?retryWrites=true&w=majority";
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 client.connect(err => {
   console.log('connected to jaxson database!')});
